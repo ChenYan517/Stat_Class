f307ac0768ebc2d9bbf09aa7240830ef825fdee9
ChenYan517 <yolanda.chan517@gmail.com>
2017-10-03 12:45:43 +0800

    Try to push another repository.

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ print "Test for pushing file to github."
 print "Tired of pushing files...Wanna cry."
 
 print "Congratulations!Can we push a file renewed?Let's have a try."
+
+print "Wow cooool! We get it! If we push to another repository, how will it be?"
