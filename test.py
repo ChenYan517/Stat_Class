dc139489881247b4915635a87f264a3545ef3c42
ChenYan517 <yolanda.chan517@gmail.com>
2017-10-03 12:50:53 +0800

    Turn to New branch: Test

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ print "Tired of pushing files...Wanna cry."
 
 print "Congratulations!Can we push a file renewed?Let's have a try."
 
-print "Wow cooool! We get it! If we push to another repository, how will it be?"
+print "Wow cooool! We get it! If we push to another repository, how will it be?Let's see."
