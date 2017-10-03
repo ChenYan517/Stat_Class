a8e627bdff4b2c9592584e3ce88b3f2cb4a2cae7
ChenYan517 <yolanda.chan517@gmail.com>
2017-10-03 13:13:50 +0800

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-print "Test for pushing file to github."
-print "Tired of pushing files...Wanna cry."
-
-print "Congratulations!Can we push a file renewed?Let's have a try."
-
-print "Wow cooool! We get it! If we push to another repository, how will it be?Let's see."
